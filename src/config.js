218aed8398aecd5e0b5aef21466d9388dfa4c306
Danny Moore <Danny@DocCapsule.com>
2020-04-04 18:11:06 +0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://3jjk4b8gpj.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_EZRuGdkNp",
+      APP_CLIENT_ID: "7m7g9fhjtllt87s94haca99u5v",
+      IDENTITY_POOL_ID: "eu-central-1:8ea6952b-11c3-4834-9d9c-22177915c9a1"
+    }
+  };
